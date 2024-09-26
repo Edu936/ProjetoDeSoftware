6a8682894382533b162f4ea88e13cefd12df728d
Eduardo Vieira <edu.vieira936@gmail.com>
2024-09-26 13:07:52 -0300

    Foi criado VeiculoController com o metodo paginaDeCadastro

--- app/views/components/atendimento-form/form-veiculo.php
+++ app/views/components/atendimento-form/form-veiculo.php
@@ -1,14 +1,1 @@
-<div class="card">
-    <h1>Buscar Cliente</h1>
-    <form action="" method="post" autocomplete="off">
-        <div class="dados">
-            <label for="user">Nome do Cliente:</label><br />
-            <input type="text" name="user" id="user" placeholder="Usuário" required />
-        </div>
-        <div class="submit">
-            <input type="reset" value="Limpar" />
-            &nbsp;
-            <input type="submit" value="Buscar" />
-        </div>
-    </form>
-</div>
+<h1>Telas De Cadastro de Veiculo</h1>
