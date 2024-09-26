a10ab01763c707348df834cfe31f9b286b771835
Eduardo Vieira <edu.vieira936@gmail.com>
2024-09-26 14:14:14 -0300

    retirado css que não era mais usado

--- app/views/controle.php
+++ app/views/controle.php
@@ -3,7 +3,6 @@
 <?php $this->start('css') ?>
 <link rel="stylesheet" href="/style/master.css">
 <link rel="stylesheet" href="/style/menu.css">
-<link rel="stylesheet" href="/style/controle.css">
 <link rel="stylesheet" href="/style/tables.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <?php $this->stop() ?>
