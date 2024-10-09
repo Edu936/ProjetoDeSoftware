e7e6efd4d7c05644c16b77ba90cd7415163bbd6a
Eduardo Vieira <edu.vieira936@gmail.com>
2024-10-08 22:38:43 -0300

    finalizado metodos js

--- public/javascript/paginas.js
+++ public/javascript/paginas.js
@@ -1,27 +1,139 @@
-function buscarCidade() {
-    const input = document.querySelector('#buscarCidade').value;
-    window.location.href = `/cidade/buscar/${input}`;
+// Metodos De Busca
+function buscarCidade(value) {
+    window.location.href = `/cidade/buscar/${value}`;
 }
 
-function buscarCliente() {
-    const option = document.querySelector('#cliente');
-    if(option.value != "Selecione o Cliente"){
-        window.location.href = `/cliente/buscar/${option.value}`;
-    }
+function buscarProduto(value) {
+    window.location.href = `/produto/buscar/${value}`;
 }
 
+function buscarServico(value) {
+    window.location.href = `/servico/buscar/${value}`;
+}
+
+function buscarCliente(value) {
+    window.location.href = `/cliente/buscar/${value}`;
+}
+
+function buscarFornecedor(value) {
+    window.location.href = `/fornecedor/buscar/${value}`;
+}
+
+function buscarOrcamento(value) {
+    window.location.href = `/orcamento/buscar/${value}`;
+}
+
+function buscarPedido(value) {
+    window.location.href = `/pedido/buscar/${value}`;
+}
+
+function buscarVeiculo(value) {
+    window.location.href = `/pedido/buscar/${value}`;
+}
+
+// Metodos De Edição
 function editarCidade(value) {
     window.location.href = `/cidade/editar/${value}`;
+}
+
+function editarOrcamento(value) {
+    window.location.href = `/orcamento/editar/${value}`;
+}
+
+function editarPedido(value) {
+    window.location.href = `/pedido/editar/${value}`;
+}
+
+function editarCliente(value) {
+    window.location.href = `/cliente/editar/${value}`;
+}
+
+function editarVeiculo(value) {
+    window.location.href = `/veiculo/editar/${value}`;
+}
+
+function editarProduto(value) {
+    window.location.href = `/produto/editar/${value}`;
+}
+
+function editarServico(value) {
+    window.location.href = `/servico/editar/${value}`;
+}
+
+function editarFornecedor(value) {
+    window.location.href = `/fornecedor/editar/${value}`;
+}
+
+// Medotos de Exclusão
+function excluirCidade(value) {
+    window.location.href = `/cidade/excluir/${value}`;
 }
 
 function excluirCidade(value) {
     window.location.href = `/cidade/excluir/${value}`;
 }
 
-function pagina(pagina) {
-    window.location = pagina;
+function excluirProduto(value) {
+    window.location.href = `/cidade/excluir/${value}`;
 }
 
-function paginaDeLogin() {
-    window.location = `/`;
+function excluirServico(value) {
+    window.location.href = `/cidade/excluir/${value}`;
+}
+
+function excluirCliente(value) {
+    window.location.href = `/cidade/excluir/${value}`;
+}
+
+function excluirFornecedor(value) {
+    window.location.href = `/cidade/excluir/${value}`;
+}
+
+function excluirOrcamento(value) {
+    window.location.href = `/cidade/excluir/${value}`;
+}
+
+function excluirPedido(value) {
+    window.location.href = `/cidade/excluir/${value}`;
+}
+
+// Metodo de Seleção
+function selecionarOrcamento() {
+    
+}
+
+function selecionarPediod() {
+    
+}
+
+function selecionarCidade() {
+    
+}
+
+function selecionarCliente() {
+    const option = document.querySelector('#cliente');
+    if(option.value != "Selecione o Cliente"){
+        window.location.href = `/cliente/buscar/${option.value}`;
+    }
+}
+
+function selecionarVeiculo() {
+
+}
+
+function selecionarProduto() {
+
+}
+
+function selecionarServico() {
+
+}
+
+function selecionarFornecedor() {
+
+}
+
+// Metodo aciliar de Pagina
+function pagina(pagina) {
+    window.location.href = pagina;
 }
